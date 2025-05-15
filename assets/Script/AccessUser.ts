50866956eff2ddbc0f97ece00c5c35786d25a793
Joanne <joanne.zh2015@gmail.com>
2025-05-15 15:46:47 +0800

    add floor to menu

---
+++ assets/Script/AccessUser.ts
@@ -0,0 +1,34 @@
+const {ccclass, property} = cc._decorator;
+
+export default class AccessUser extends cc.Component {
+    private static _instance: AccessUser = null;
+
+    public static get instance(): AccessUser {
+        return this._instance;
+    }
+
+    onLoad() {
+        if (AccessUser._instance === null) {
+            AccessUser._instance = this;
+            cc.game.addPersistRootNode(this.node);
+        } else {
+            this.node.destroy();
+        }
+    }
+
+    public async saveUser(userid: string, email: string, username: string) {
+        const firebase = cc.find("FirebaseService").getComponent("FirebaseService").getFirebase();
+        const db = firebase.firestore();
+
+        try {
+            await db.collection("users").doc(userid).set({
+                email: email,
+                username: username,
+            }, { merge: true });
+            cc.log("User saved successfully");
+        } catch (error) {
+            cc.error("Error saving user: ", error);
+            throw error;
+        }
+    }
+}
--- assets/Script/SignUp.ts
+++ assets/Script/SignUp.ts
@@ -8,9 +8,36 @@ export default class SignUp extends cc.Component {
     @property(cc.Button)
     EnterButton: cc.Button = null;
 
+    @property(cc.EditBox)
+    emailInput: cc.EditBox = null;
+    @property(cc.EditBox)
+    usernameInput: cc.EditBox = null;
+    @property(cc.EditBox)
+    passwordInput: cc.EditBox = null;
+
     onBack () {
         cc.log("Back button clicked");
         cc.director.loadScene('Start');
+    }
+
+    onEnter () {
+        cc.log("Enter button clicked");
+
+        if(!this.emailInput || !this.usernameInput || !this.passwordInput) {
+            cc.warn("Some is missing")
+            return;
+        }
+
+        const email = this.emailInput.string;
+        const username = this.usernameInput.string;
+        const password = this.passwordInput.string;
+
+        if(!email || !username || !password) {
+            cc.warn("Some is empty")
+            return;
+        }
+
+        this.signUpFirebase(email, username, password);
     }
 
     onLoad () {
@@ -30,5 +57,28 @@ export default class SignUp extends cc.Component {
 
     }
 
-    // update (dt) {}
+    signUpFirebase(email: string, username: string, password: string) {
+        const firebase = cc.find("FirebaseService").getComponent("FirebaseService").getFirebase();
+        if (firebase) {
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+                .then((userCredential) => {
+                    const user = userCredential.user;
+                    cc.log("User signed up:", user);
+
+                    const accessUser = cc.find("AccessUser").getComponent("AccessUser");
+                    if (accessUser) {
+                        accessUser.saveUser(user.uid, email, username);
+                    }
+
+                    cc.director.loadScene('Menu');
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    cc.error("Error signing up:", errorCode, errorMessage);
+                });
+        } else {
+            cc.error("Firebase is not initialized.");
+        }
+    }
 }
